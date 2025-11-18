1aef55dc65facd97b2572745c581810757d65a9d
Samuel Hellstrøm <samuel.hellstrom@netlight.com>
2025-11-18 15:46:25 +0100

    Refactor GenAiService to accept API key as a parameter, enhance content generation with file search capabilities, and return structured response including grounding metadata.

--- src/services/gen_ai_service.py
+++ src/services/gen_ai_service.py
@@ -1,18 +1,43 @@
-import os
 from google import genai
+from google.genai import types
+from src.models.generate_content import GenerateContentResponse
 
 
 class GenAiService():
-    def __init__(self):
-        api_key = os.getenv("GEMINI_API_KEY")
-        if not api_key:
-            raise ValueError("GEMINI_API_KEY environment variable not set.")
+    def __init__(self, gemini_api_key: str):
+        self.client = genai.Client(api_key=gemini_api_key)
+        self.model = "gemini-2.5-flash-lite"
 
-        self.client = genai.Client(api_key=api_key)
+    def generate_content(self, query: str, file_search_store_name: str = None) -> GenerateContentResponse:
+        system_instruction = ""
+        tools = []
 
-    def generate_content(self, query: str) -> str:
-        response = self.client.models.generate_content(
-            model="gemini-2.5-flash-lite",
-            contents=query,
+        if file_search_store_name:
+            system_instruction = """
+            Your sole purpose is answer questions based on information from tools provided.
+            Always answer consise and to the point, never make up your own information or assumptions.
+            If the tools do not return an answer, you must respond 'I don't know'.
+            """
+            tools.append(types.Tool(
+                file_search=types.FileSearch(
+                    file_search_store_names=[file_search_store_name]
+                )
+            ))
+
+        config = types.GenerateContentConfig(
+            system_instruction=system_instruction,
+            tools=tools,
         )
-        return response.text
+
+        response = self.client.models.generate_content(
+            model=self.model,
+            contents=query,
+            config=config
+        )
+
+        content = response.text
+        grounding_metadata = None
+        if response.candidates and len(response.candidates) > 0:
+            grounding_metadata = response.candidates[0].grounding_metadata
+
+        return GenerateContentResponse(content=content, grounding_metadata=grounding_metadata)
