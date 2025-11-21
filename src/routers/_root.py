690d3c34fe42d2f9dead93ce85cec1a0731f40bd
Samuel Hellstrøm <samuel.hellstrom@netlight.com>
2025-11-21 11:20:18 +0100

    Add root router with endpoints for root access and content generation. Implement validation for request body and error handling for service interactions.

--- src/routers/_root.py
+++ src/routers/_root.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from google.genai.errors import ClientError
 from src.dependencies.services import get_file_search_store_service, get_gen_ai_service
-from src.models.generate_content import GenerateContentResponse
+from src.models.generate_content import GenerateContentRequest, GenerateContentResponse
 from src.services.file_search_store_service import FileSearchStoreService
 from src.services.gen_ai_service import GenAiService
 
@@ -15,22 +15,21 @@ def get_root():
 
 @root_router.post("/generate_content", response_model=GenerateContentResponse)
 def generate_content(
-    query: str,
-    file_search_store_name: str = None,
+    body: GenerateContentRequest,
     file_search_store_service: FileSearchStoreService = Depends(
         get_file_search_store_service),
     gen_ai_service: GenAiService = Depends(get_gen_ai_service),
 ) -> GenerateContentResponse:
-    if not query:
+    if not body.query:
         raise HTTPException(status_code=400, detail="Query is required")
-    if not file_search_store_name:
+    if not body.file_search_store_name:
         print("No file search store name provided")
-        return gen_ai_service.generate_content(query)
+        return gen_ai_service.generate_content(body.query)
 
     try:
         file_search_store = file_search_store_service.get_file_search_store(
-            file_search_store_name)
-        return gen_ai_service.generate_content(query, file_search_store.name)
+            body.file_search_store_name)
+        return gen_ai_service.generate_content(body.query, file_search_store.name)
     except ClientError as e:
         print(e)
         raise HTTPException(status_code=400, detail=e.message) from e
